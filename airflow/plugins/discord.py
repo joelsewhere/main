a4f6101ef85de134e93eedd43699da97928516aa
Joél Collins <joelsewhere@gmail.com>
2022-11-25 16:44:33 +0100

    Add token and generate bot instance

--- airflow/plugins/discord.py
+++ airflow/plugins/discord.py
@@ -1,4 +1,3 @@
-import os
 from src.discord.bot import bot_factory
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.decorators import apply_defaults
@@ -6,8 +5,6 @@ from airflow.operators.python_operator import PythonOperator
 
 
 class DiscordOperator(PythonOperator):
-
-    TOKEN = os.environ['DiscordBot']
 
     @apply_defaults
     def __init__(
@@ -20,7 +17,7 @@ class DiscordOperator(PythonOperator):
         self.bot_components = bot_components
 
     def execute(self, context):
-        bot = bot_factory(self.bot_components)
+        bot = bot_factory(self.bot_components)()
         self.op_args = [bot] + self.op_args
         callable_return = super().execute(context)
         return callable_return
